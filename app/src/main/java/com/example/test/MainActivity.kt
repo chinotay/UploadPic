c85fc6218636be591ca7afa41cd98e18f683a7da
chinotay <taykhenghoe@gmail.com>
2020-01-09 20:55:06 +0800

    Show gif or picture on show upload as recycler view

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -34,18 +34,18 @@ class AppGlideModule : AppGlideModule()
 
 class MainActivity : AppCompatActivity() {
 
-    private var PICK_IMAGE_REQUEST =1
-    private var mButtonChooseImage: Button? =null
-    private var mButtonUpload:Button?=null
+    private var PICK_IMAGE_REQUEST = 1
+    private var mButtonChooseImage: Button? = null
+    private var mButtonUpload: Button? = null
     private var mTextViewShowUpload: TextView? = null
     private var mEditTextFileName: EditText? = null
     private var mImageView: ImageView? = null
     private var mProgressBar: ProgressBar? = null
 
-    private var mImageUri: Uri?=null
+    private var mImageUri: Uri? = null
 
     private var aStorageRef: StorageReference? = null
-    private var aDatabaseReference: DatabaseReference?= null
+    private var aDatabaseReference: DatabaseReference? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,67 +53,90 @@ class MainActivity : AppCompatActivity() {
         //initialize variable
         aStorageRef = FirebaseStorage.getInstance().getReference("workout")
         aDatabaseReference = FirebaseDatabase.getInstance().getReference("workout")
-        mButtonChooseImage=findViewById(R.id.button_choose_gif)
-        mButtonUpload=findViewById((R.id.button_upload))
-        mTextViewShowUpload=findViewById(R.id.text_view_show_uploads)
-        mEditTextFileName=findViewById(R.id.edit_text_file_name)
-        mImageView=findViewById(R.id.image_view)
-        mProgressBar=findViewById(R.id.progress_bar)
+        mButtonChooseImage = findViewById(R.id.button_choose_gif)
+        mButtonUpload = findViewById((R.id.button_upload))
+        mTextViewShowUpload = findViewById(R.id.text_view_show_uploads)
+        mEditTextFileName = findViewById(R.id.edit_text_file_name)
+        mImageView = findViewById(R.id.image_view)
+        mProgressBar = findViewById(R.id.progress_bar)
 
         //when click choose file
-        mButtonChooseImage!!.setOnClickListener{
+        mButtonChooseImage!!.setOnClickListener {
             openFileChooser()
         }
-        mButtonUpload!!.setOnClickListener{
+        mButtonUpload!!.setOnClickListener {
             uploadFile()
         }
+        mTextViewShowUpload!!.setOnClickListener {
+            openImagesActivity()
         }
+    }
+
     private fun openFileChooser() {
         //open a file chooser to allow user choose photo from file
         val intent = Intent(Intent.ACTION_GET_CONTENT)
         intent.type = "image/*"
         startActivityForResult(intent, PICK_IMAGE_REQUEST)
     }
+
     private fun getFileExtension(uri: Uri): String? {
         //get file extension from image(in our case is gif)
-         var cR:ContentResolver = contentResolver
-        var mime:MimeTypeMap = MimeTypeMap.getSingleton()
+        var cR: ContentResolver = contentResolver
+        var mime: MimeTypeMap = MimeTypeMap.getSingleton()
         return mime.getExtensionFromMimeType(cR.getType(uri))
     }
 
     private fun uploadFile() {
-        if(mImageUri != null){
-            var fileReference:StorageReference? = aStorageRef?.child(
-                "." + System.currentTimeMillis() +this.getFileExtension(mImageUri!!) // the reference will be like 123456.gif
+        if (mImageUri != null) {
+            var fileReference: StorageReference? = aStorageRef?.child(
+                "." + System.currentTimeMillis() + this.getFileExtension(mImageUri!!) // the reference will be like 123456.gif
             )
-            fileReference?.putFile(mImageUri!!)
-                ?.addOnSuccessListener{
-                    Toast.makeText(this@MainActivity, "Upload successfully",Toast.LENGTH_LONG).show()
-                    var upload:workout = workout(
-                        mEditTextFileName?.text.toString().trim(),
-                        it.uploadSessionUri.toString())
+            val uploadTask = fileReference?.putFile(mImageUri!!)                //declare upload task
+            val urlTask = uploadTask?.continueWithTask { task ->                //declare a task for upload correct url
+                if (!task.isSuccessful) {
+                    task.exception?.let {
+                        throw it
+                    }
+                }
+                fileReference?.downloadUrl
+            }?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val downloadUri = task.result               //Url is download link
 
+                    var upload: workout = workout(
+                        mEditTextFileName?.text.toString().trim(),
+                        downloadUri.toString()                          //download url is save to retireve next time
+                    )
+                    //it.uploadSessionUri.toString())
                     var uploadId = aDatabaseReference?.push()?.key
                     if (uploadId != null) {
                         aDatabaseReference?.child(uploadId)?.setValue(upload)
                     }
-                    }
-                ?.addOnFailureListener{
-                    Toast.makeText(this@MainActivity,it.message,Toast.LENGTH_SHORT).show()
+                }
+            }
+            uploadTask?.addOnSuccessListener {
+                Toast.makeText(this@MainActivity, "Upload successfully", Toast.LENGTH_LONG).show()
+                }
+                ?.addOnFailureListener {
+                    Toast.makeText(this@MainActivity, it.message, Toast.LENGTH_SHORT).show()
                 }
                 ?.addOnProgressListener {
-                    var progress = (100.0 * it.bytesTransferred /it.totalByteCount)
+                    var progress = (100.0 * it.bytesTransferred / it.totalByteCount)
                     mProgressBar?.progress = progress.toInt()
                 }
-        }else{
-            Toast.makeText(this@MainActivity , "No file selected.",
-                Toast.LENGTH_SHORT).show()
+
+        } else {
+            Toast.makeText(
+                this@MainActivity, "No file selected.",
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if (requestCode ==PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data!=null){
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null) {
             mImageUri = data.data
             //use picasso show image(but gif cannot move)
             //Picasso.get().load(mImageUri).into(mImageView)
@@ -124,5 +147,9 @@ class MainActivity : AppCompatActivity() {
                 .into(mImageView)//show on image view
         }
 
+    }
+
+    private fun openImagesActivity() {
+        startActivity(Intent(this, gifActivity::class.java))
     }
 }
